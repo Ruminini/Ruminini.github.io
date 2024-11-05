ddac885fa2b7a6c2f306458dd5a89096b166a299
Ruminini <63514002+Ruminini@users.noreply.github.com>
2024-11-05 00:28:25 -0300

    Feat Red outline on empty field & Fixed section overlap

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const fMessage = document.getElementById("fMessage");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	console.log(fName.value, fEmail.value, fMessage.value);
-	if (fName.value === "" || fEmail.value === "" || fMessage.value === "") {
-		alert("Please fill out all fields.");
-	} else {
+	if (fName.value === "") {errorField(fName);}
+	else if (fEmail.value === "") {errorField(fEmail);}
+	else if (fMessage.value === "") {errorField(fMessage);}
+	else {
 		const url = `https://docs.google.com/forms/d/e/1FAIpQLSdJmv7jU49t4FcWpbdjcycWiGO0sx9xPji1Q8s8QfLva4yc6w/formResponse?submit=Submit&usp=pp_url&entry.1137438515=${fName.value}&entry.32220053=${fEmail.value}&entry.1023053542=${fMessage.value}`;
 		window.open(url, "_blank").focus();
 
@@ -17,3 +17,12 @@ form.addEventListener("submit", (e) => {
 		fMessage.value = "";
 	}
 });
+
+const timeouts = {};
+function errorField(field) {
+	if (timeouts[field.id]) {clearTimeout(timeouts[field.id]);}
+	field.classList.add("error");
+	timeouts[field.id] = setTimeout(() => {
+		field.classList.remove("error");
+	}, 5000);
+}
